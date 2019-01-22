eb7c63b62071c5a012755ba6001bf2305e9ac7d7
Ubuntu <ubuntu@ip-10-0-0-14.ec2.internal>
2019-01-22 20:47:17 +0000

    In the process of fixing the configparser

--- kafka/test_kafka_producer.py
+++ kafka/test_kafka_producer.py
@@ -17,18 +17,10 @@ class StdOutListener(StreamListener):
         print (data)
         return True
     def on_error(self, status):
-        print (status)
+        print ("Status - ", status)
 
 
 #This is a basic listener that just prints received tweets to stdout.
-class StdOutListener(StreamListener):
-
-    def on_data(self, data):
-        print data
-        return True
-
-    def on_error(self, status):
-        print status
 
 
 if __name__ == '__main__':
