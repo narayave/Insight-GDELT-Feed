b412634554e32df2d6d8b08dce8d7c25fb830051
Vedanth Narayanan <vedanth.nd@gmail.com>
2019-01-22 17:16:42 -0500

    Hello World for reading S3 bucket gdelt-open-data

---
+++ gdelt/test_gdelt_query.py
@@ -0,0 +1,16 @@
+import boto3
+
+# Let's use Amazon S3
+s3 = boto3.resource('s3', region_name='us-east-1')
+
+bucket_name = 'gdelt-open-data'
+
+bucket = s3.Bucket(bucket_name)
+
+i = 0
+for object in bucket.objects.all():
+    print(object)
+    
+    i += 1
+    if i > 100:
+        break
