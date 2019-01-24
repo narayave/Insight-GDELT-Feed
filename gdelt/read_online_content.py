5676d6dff12d4347f5a5ed2bba3149ea3390fb82
Vedanth Narayanan <vedanth.nd@gmail.com>
2019-01-24 18:47:59 -0500

    Able to read S3 file contents

--- gdelt/read_online_content.py
+++ gdelt/read_online_content.py
@@ -1,20 +1,19 @@
 import urllib2
+import boto3
+import botocore
+from smart_open import smart_open
 
-
-def read_online_txt():
+def get_target_filename():
 
     # Super important link, updated every 15 minutes
     target_url = 'http://data.gdeltproject.org/gdeltv2/lastupdate.txt'
     target_file = ''
     data = urllib2.urlopen(target_url)
-    print type(data)
     for line in data:
         target_file = line
         break
 
-    target_file = get_filename(target_file)
-
-    print 'Done'
+    return target_file
 
 
 def get_filename(location):
@@ -25,12 +24,25 @@ def get_filename(location):
     print target_link    
     target_file = target_link.split("/")[-1]
     print target_file
-    target_file = target_file.replace(".zip", "")
+    target_file = target_file.replace(".zip\n", "")
     print target_file
 
     return target_file
 
 
+def read_s3_contents(target_file):
+
+    for line in smart_open('s3://gdelt-open-data/v2/events/'+target_file, 'rb'):
+        print line.decode('utf8')
+
+    print 'Done'
+
+
 if __name__ == '__main__':
+
     print 'Reporting for duty'
-    read_online_txt()
+
+    target_file = get_target_filename()
+    target_filename = get_filename(target_file)
+
+    read_s3_contents("20190124233000.export.csv")
