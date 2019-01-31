c17308b9bbe1c012c305d402a4a33a57028ba3b8
Vedanth Narayanan <vedanth.nd@gmail.com>
2019-01-31 20:52:20 +0000

    Able to query the database

--- flask/app.py
+++
@@ -1,4 +0,0 @@
-from click_web import create_click_web_app
-import example_command
-
-app = create_click_web_app(example_command, example_command.cli)
--- flask/flaskexample/views.py
+++ flask/flaskexample/views.py
@@ -1,12 +1,35 @@
-from flaskexample import app
 from flask import render_template
+from flaskexample import app
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+import pandas as pd
+import psycopg2
+
+
+user = 'gres' #add your username here (same as previous postgreSQL)                      
+host = 'localhost'
+dbname = 'gdelt'
+
+db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
+con = None
+con = psycopg2.connect(database = dbname, user = user)
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-	user = {'nickname': 'Vee'}
-	return render_template("index.html",
-		title= 'Home',
-		user = user)
+    return render_template("index.html",
+       title = 'Home', user = { 'nickname': 'Vee' },
+       )
 
+@app.route('/db')
+def events_page():
+    sql_query = """
+                SELECT * FROM gdelt_events;          
+                """
+
+    query_results = pd.read_sql_query(sql_query,con)
+    events = ""
+    for i in range(0,10):
+        events += (query_results.iloc[i]['actor1geo_fullname'])
+        events += "<br>"
+    return events
