bf85619e0a30adbe884395ab6b49f92fe2c3b505
Vedanth Narayanan <vedanth.nd@gmail.com>
2019-02-03 20:04:30 +0000

    Spark job runs and prints

--- spark/test_spark.py
+++ spark/test_spark.py
@@ -15,27 +15,24 @@ if __name__ == "__main__":
 
 
 
-#    spark = SparkSession.builder \
-#    	.appName("GDELT-News") \
-#    	.config("spark.executor.memory", "1gb") \
-#    	.getOrCreate()
+    sc = SparkSession.builder \
+	.master("spark://ec2-18-235-99-234.compute-1.amazonaws.com:7077") \
+	.appName("GDELT-News") \
+	.config("spark.executor.memory", "2gb") \
+	.getOrCreate()
 
-    conf = (SparkConf().setMaster("spark://ec2-18-235-99-234.compute-1.amazonaws.com:7077").setAppName("GDELT-News").set("spark.executor.memory", "6gb"))
+    sqlcontext = SQLContext(sc)
+    gdelt_bucket = "s3n://gdelt-open-data/v2/events/20180410190000.export.csv" #201901*.export.csv" #	20180410190000.export.csv"
 
-    sc = SparkContext(conf = conf) #appName="GDELT-News")
-    sqlContext = SQLContext(sc)
-    gdelt_bucket = "s3n://gdelt-open-data/v2/events/2019*.export.csv" #	20180410190000.export.csv"
-    #gdelt_bucket = "events.csv"
-
-    df = sqlContext.read \
+    df = sqlcontext.read \
     	.format('com.databricks.spark.csv') \
     	.options(header='false') \
     	.options(delimiter="\t") \
-		.load(gdelt_bucket, schema = gdelt_schema.gdeltSchema)
+	.load(gdelt_bucket, schema = gdelt_schema.gdeltSchema)
 
-    sqlContext.registerDataFrameAsTable(df, 'temp')
+    sqlcontext.registerDataFrameAsTable(df, 'temp')
 
-    df_clean = sqlContext.sql("""SELECT GLOBALEVENTID,
+    df_clean = sqlcontext.sql("""SELECT GLOBALEVENTID,
                               CAST(SQLDATE AS INTEGER),
                               Actor1Name,
                               SOURCEURL
@@ -44,8 +41,8 @@ if __name__ == "__main__":
 
     df_news = df_clean.select('GLOBALEVENTID','SQLDATE','Actor1Name', 'SOURCEURL')
 
-    #df_news.repartition(1000, 'GLOBALEVENTID')
-
+    df_news.repartition(1000, 'GLOBALEVENTID')
+    print 'I did stuff'
 
     df_news.show(df_news.count())
 
