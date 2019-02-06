d02a1b92584107954257c4c7c3f1b5f9802f0f7c
Ubuntu <ubuntu@ip-10-0-0-8.ec2.internal>
2019-02-06 18:24:39 +0000

    Aggregating approriate events, almost ready for postgresql sink

--- spark/test_spark.py
+++ spark/test_spark.py
@@ -6,6 +6,7 @@ from pyspark.sql import SQLContext, SparkSession, Row
 from pyspark.sql.types import StringType, DoubleType, IntegerType
 from pyspark.sql.functions import udf
 from pyspark.sql.functions import col
+from pyspark.sql import functions as F
 
 import gdelt_schema
 import configparser
@@ -13,13 +14,10 @@ import configparser
 
 if __name__ == "__main__":
 
-
-
     sc = SparkSession.builder \
-	.master("spark://ec2-18-235-99-234.compute-1.amazonaws.com:7077") \
-	.appName("GDELT-News") \
-	.config("spark.executor.memory", "2gb") \
-	.getOrCreate()
+        .master("spark://ec2-54-84-194-198.compute-1.amazonaws.com:7077") \
+        .appName("GDELT-News") \
+        .config("spark.executor.memory", "2gb").getOrCreate()
 
     sqlcontext = SQLContext(sc)
     gdelt_bucket = "s3n://gdelt-open-data/v2/events/20180410190000.export.csv"
@@ -34,24 +32,37 @@ if __name__ == "__main__":
 
     df_clean = sqlcontext.sql("""SELECT GLOBALEVENTID,
                               CAST(SQLDATE AS INTEGER),
+                              EventCode,
+                              Actor1Code,
                               Actor1Name,
+                              Actor1Type1Code,
+                              ActionGeo_FullName,
+                              ActionGeo_CountryCode,
 			      Actor1Geo_CountryCode,
+                              ActionGeo_ADM1Code,
 			      Actor2Name,
 			      Actor2Geo_CountryCode,
-                              CAST(GoldsteinScale AS NUMERIC),
-			      CAST(AvgTone as NUMERIC),
-			      SOURCEURL
+                              GoldsteinScale,
+			      AvgTone
                             from temp
                             """)
 
-    df_news = df_clean.select('GLOBALEVENTID','SQLDATE','Actor1Geo_CountryCode', 'Actor2Geo_CountryCode', 'GoldsteinScale', 'AvgTone')
+    df_news = df_clean.select('GLOBALEVENTID','SQLDATE','EventCode', 'Actor1Code','Actor1Type1Code','ActionGeo_FullName','ActionGeo_CountryCode','ActionGeo_ADM1Code','Actor1Geo_CountryCode','GoldsteinScale', 'AvgTone')
 
     #df_news.repartition(1000, 'GLOBALEVENTID')
-    print 'I did stuff'
+    df_news.show(10)
+
+    df_news = df_news.filter(df_news.ActionGeo_CountryCode == 'US')
+    df_news = df_news.filter(df_news.Actor1Code != 'null')
+
+    #df_news = df_news.groupby('ActionGeo_FullName', 'EventCode').agg(F.collect_list(''))
+    df_news = df_news.groupby('ActionGeo_CountryCode', 'Actor1Type1Code','ActionGeo_ADM1Code')
+    print df_news.count().show()
+    #df_news = df_news.rdd.map(lambda)
 
     # Dataframe object does not have the map attribute, which is why rdd needs to be
     # 	called firstd..reduceByKey(lambda a, b: a + b)
-    df_news = df_news.rdd.map(lambda line: (line[1], 1)).reduceByKey(lambda a, b: a + b)
+    df_news = df_news.rdd.map(lambda line: (line[2], 1)).reduceByKey(lambda a, b: a + b)
 
     res = df_news.collect()
 
