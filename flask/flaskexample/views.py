064384c256284879c937e92a1d8b69342531d847
Vedanth Narayanan <vedanth.nd@gmail.com>
2019-02-07 12:55:00 -0500

    Front end dev, fixing inputs

--- flask/flaskexample/views.py
+++ flask/flaskexample/views.py
@@ -5,15 +5,20 @@ from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
 import psycopg2
 from flask import request
+from six.moves import configparser
 
+config = configparser.ConfigParser()
+# TODO: Make sure to read the correct config.ini file on AWS workers
+config.read('/home/vee/repos/Insight-GDELT-Feed/flask/flaskexample/config.ini')
+dbname = config.get('dbauth', 'dbname')
+dbuser = config.get('dbauth', 'user')
+dbpass = config.get('dbauth', 'password')
+dbhost = config.get('dbauth', 'host')
+dbport = config.get('dbauth', 'port')
 
-user = 'gres' #add your username here (same as previous postgreSQL)
-host = 'ec2-18-235-99-234.compute-1.amazonaws.com'
-dbname = 'gdelt'
-
-db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
+db = create_engine('postgres://%s%s/%s'%(dbuser,dbhost,dbname))
 con = None
-con = psycopg2.connect(database = dbname, host = host, user = user, password = 'password')
+con = psycopg2.connect(database = dbname, host = dbhost, user = dbuser, password = dbpass)
 
 @app.route('/')
 @app.route('/index')
@@ -26,7 +31,7 @@ def index():
 @app.route('/db')
 def events_page():
     sql_query = """
-                SELECT * FROM gdelt_events;
+                SELECT * FROM events;
                 """
 
     query_results = pd.read_sql_query(sql_query,con)
@@ -40,7 +45,7 @@ def events_page():
 @app.route('/db_fancy')
 def events_page_fancy():
     sql_query = """
-               SELECT * FROM gdelt_events;
+               SELECT * FROM events;
 
                 """
     query_results=pd.read_sql_query(sql_query,con)
@@ -62,10 +67,12 @@ def location_output():
 
 	#pull 'birth_month' from input field and store it
 	loc = request.args.get('location')
+	checks = request.args.getlist('check_list[]')
+	print checks
 
 	#just select the Cesareans  from the birth dtabase for the month that the user inputs
 	query = "SELECT globaleventid, sqldate, actor1name, \
-            source_url FROM gdelt_events WHERE actor1countrycode='%s' or actor2countrycode='%s'" %(loc, loc)
+            sourceurl FROM events WHERE actor1geo_countrycode='%s' ORDER BY sqLdate DESC LIMIT 10" %(loc)
 	print(query)
 
 	query_results=pd.read_sql_query(query,con)
@@ -75,8 +82,9 @@ def location_output():
 		items.append(dict(eventid=query_results.iloc[i]['globaleventid'], \
                         date=query_results.iloc[i]['sqldate'], \
                         actor_name=query_results.iloc[i]['actor1name'], \
-                        sourceurl=query_results.iloc[i]['source_url']))
+                        sourceurl=query_results.iloc[i]['sourceurl']))
 
 		#the_result = ''
 
-	return render_template("test_page.html", items = items) #, the_result = the_result)
+	return render_template("output.html", items = items) #, the_result = the_result)
+
