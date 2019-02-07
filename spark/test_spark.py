077f45094e0a5b9fc55128dc7cf1d50a6715ab55
Vedanth Narayanan <vedanth.nd@gmail.com>
2019-02-07 13:52:48 +0000

    Can write aggregated data to postgres db

--- spark/test_spark.py
+++ spark/test_spark.py
@@ -10,18 +10,41 @@ from pyspark.sql import functions as F
 from pyspark.sql.functions import UserDefinedFunction
 
 import gdelt_schema
-import configparser
+from six.moves import configparser
+
+def postgres_dump(config, data_frame):
+
+    config.read('/home/ubuntu/Insight-GDELT-Feed/spark/config.ini')
+    dbname = config.get('dbauth', 'dbname')
+    dbuser = config.get('dbauth', 'user')
+    dbpass = config.get('dbauth', 'password')
+    dbhost = config.get('dbauth', 'host')
+    dbport = config.get('dbauth', 'port')
+
+    url = "jdbc:postgresql://"+dbhost+":"+dbport+"/"+dbname
+    properties = {
+                "driver": "org.postgresql.Driver",
+                "user": dbuser,
+                "password": dbpass
+                }
+
+    mode = 'append'
+    data_frame.write.jdbc(url=url, table="tmp_query2", mode=mode, properties=properties)
 
 
 if __name__ == "__main__":
 
+    config = configparser.ConfigParser()
+
     sc = SparkSession.builder \
         .master("spark://ec2-54-84-194-198.compute-1.amazonaws.com:7077") \
         .appName("GDELT-News") \
-        .config("spark.executor.memory", "4gb").getOrCreate()
+        .config("spark.executor.memory", "4gb") \
+        .config("spark.jars", "/home/ubuntu/Insight-GDELT-Feed/spark/postgresql-42.2.5.jar").getOrCreate()
 
     sqlcontext = SQLContext(sc)
     gdelt_bucket = "s3n://gdelt-open-data/v2/events/20180410190000.export.csv"
+    #gdelt_bucket = "s3n://gdelt-open-data/v2/events/20190206*.export.csv"
 
     df = sqlcontext.read \
     	.format('com.databricks.spark.csv') \
@@ -33,6 +56,8 @@ if __name__ == "__main__":
 
     df_clean = sqlcontext.sql("""SELECT GLOBALEVENTID,
                               CAST(SQLDATE AS INTEGER),
+                              MonthYear,
+                              Year,
                               EventCode,
                               Actor1Code,
                               Actor1Name,
@@ -48,50 +73,37 @@ if __name__ == "__main__":
                             from temp
                             """)
 
-    df_news = df_clean.select('GLOBALEVENTID','SQLDATE','EventCode', 'Actor1Code','Actor1Name','Actor2Name','Actor1Type1Code','ActionGeo_FullName','ActionGeo_CountryCode','ActionGeo_ADM1Code','Actor1Geo_CountryCode','GoldsteinScale', 'AvgTone')
+    df_news = df_clean.select('GLOBALEVENTID','SQLDATE','MonthYear','Year','Actor1Code','Actor1Type1Code','ActionGeo_FullName','ActionGeo_CountryCode','ActionGeo_ADM1Code','Actor1Geo_CountryCode','GoldsteinScale', 'AvgTone')
 
     #df_news.repartition(1000, 'GLOBALEVENTID')
     df_news.show(10)
 
     df_news = df_news.filter(df_news.ActionGeo_CountryCode == 'US')
     df_news = df_news.filter(df_news.Actor1Code != 'null')
+    df_news = df_news.filter(df_news.Actor1Type1Code != 'null')
 
     name = 'ActionGeo_ADM1Code'
     udf = UserDefinedFunction(lambda x: x[:2]+'-'+x[2:], StringType())
     df_news = df_news.select(*[udf(column).alias(name) if column == name else column for column in df_news.columns])
-    #df_news.show()
-
-    #df_news = df_news.rdd.map(lambda line: line['ActionGeo_ADM1Code'][:2] + '-' + line['ActionGeo_ADM1Code'][2:])
-    #df_news = df_news.collect()
-    #for val in range(10):
-    #    print df_news[val]
 
     split_col = F.split(df_news['ActionGeo_ADM1Code'],'-')
     df_news = df_news.withColumn('action_state', split_col.getItem(1))
-
     print df_news.show()
 
-    df_news = df_news.groupby('ActionGeo_CountryCode','action_state','Actor1Type1Code').agg( #F.collect('GLOBALEVENTID'),
-                                                                                                        F.collect_list('SQLDATE'),
-                                                                                                        #F.collect_list('EventCode'),
-                                                                                                        #F.col('Actor1Name'),
-                                                                                                        #F.col('Actor2Name'),
-                                                                                                        F.collect_list('ActionGeo_ADM1Code'),
-                                                                                                        F.collect_list('action_state'),
-                                                                                                        F.collect_list('ActionGeo_Fullname'),
-                                                                                                        F.avg('GoldsteinScale')) #,
-                                                                                                        #F.avg('AvgTone'))
-    print df_news.show()
-    #df_news = df_news.rdd.map(lambda)
+    df_news = df_news.groupby('action_state','Year','Actor1Type1Code').agg( F.approx_count_distinct('GLOBALEVENTID'),
+                                                                            #F.col('SQLDATE'),
+                                                                            #F.collect_list('EventCode'),
+                                                                            #F.col('Actor1Name'),
+                                                                            #F.col('Actor2Name'),
+                                                                            #F.collect_list('ActionGeo_ADM1Code'),
+                                                                            #F.collect_list('action_state'),
+                                                                            #F.collect_list('ActionGeo_Fullname'),
+                                                                            F.avg('GoldsteinScale'),
+                                                                            F.avg('AvgTone'))
 
-    # Dataframe object does not have the map attribute, which is why rdd needs to be
-    # 	called firstd..reduceByKey(lambda a, b: a + b)
-#    df_news = df_news.rdd.map(lambda line: (line[2], 1)).reduceByKey(lambda a, b: a + b)
+    print df_news.show(df_news.count())
+    print df_news.printSchema()
 
-#    res = df_news.collect()
+    postgres_dump(config, df_news)
 
-#    for val in res:
-#	print val
-
-    #df_news.show(df_news.count())
 
