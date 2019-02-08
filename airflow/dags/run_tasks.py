cb53e9e2b208d8cd87f433a9e705cf0b13c1343e
Vedanth Narayanan <vedanth.nd@gmail.com>
2019-02-08 02:50:41 +0000

    Fixed airflow dag

--- airflow/dags/run_tasks.py
+++ airflow/dags/run_tasks.py
@@ -1,25 +1,27 @@
 import datetime as dt
 from airflow import DAG
-
+import airflow
 from airflow.operators.bash_operator import BashOperator
 
 default_args = {
         'owner': 'airflow',
-        'start_date': dt.datetime.now(),
+        'start_date': airflow.utils.dates.days_ago(2),
         'retries': 2,
         'retry_delay': dt.timedelta(minutes=3)
         }
 
-with DAG('database_task',
+dag = DAG('database_task',
         default_args=default_args,
         schedule_interval='*/15 * * * *',
-        ) as dag:
+        )
 
+db_update_confirm = BashOperator(task_id='db_update_confirm',
+                                bash_command="echo 'Hey, I added new events to database'",
+                                dag=dag)
 
-    load_database = BashOperator(task_id='load_database',
-                                bash_command='python ~/Insight-GDELT-Feed/gdelt/new_event_collection.py')
+load_database = BashOperator(task_id='load_database',
+                                bash_command='python ~/Insight-GDELT-Feed/gdelt/new_event_collection.py',
+                                dag=dag)
 
-    db_update_confirm = BashOperator(task_id='db_update_confirm',
-                                bash_command="echo 'Hey, I added new events to database'")
 
 load_database >> db_update_confirm
