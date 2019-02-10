1ab072e86594982ba5d7f9b7ce53871a1679a154
Vedanth Narayanan <vedanth.nd@gmail.com>
2019-02-10 15:34:37 -0500

    Refactored code + script should succeed now

--- spark/spark_run.py
+++ spark/spark_run.py
@@ -31,8 +31,79 @@ def postgres_dump(config, data_frame):
                 }
 
     mode = 'append'
-    data_frame.write.jdbc(url=url, table="final_results_test", mode=mode, properties=properties)
+    data_frame.write.jdbc(url=url, table="final_results_test", mode=mode, \
+                properties=properties)
 
+
+def get_clean_df(sqlcontext, sql_df):
+
+    sqlcontext.registerDataFrameAsTable(sql_df, 'temp')
+
+    df_clean = sqlcontext.sql("""SELECT GLOBALEVENTID,
+                        CAST(SQLDATE AS INTEGER), MonthYear,
+                        Year, EventCode, Actor1Code, Actor1Name,
+                        Actor1Type1Code, ActionGeo_FullName,
+                        ActionGeo_CountryCode, Actor1Geo_CountryCode,
+                        ActionGeo_ADM1Code, Actor2Name,
+                        Actor2Geo_CountryCode,
+                        CAST(GoldsteinScale AS DOUBLE)
+                        from temp
+                        """)
+
+    df_select = df_clean.select('GLOBALEVENTID','SQLDATE','Year','Actor1Code',
+                                'Actor1Type1Code','ActionGeo_FullName',
+                                'ActionGeo_CountryCode', 'ActionGeo_ADM1Code',
+                                'Actor1Geo_CountryCode','GoldsteinScale')
+
+    return df_select
+
+
+def filter_df(df_news):
+    role_codes_of_interest = ["COP", "GOV", "JUD", "BUS", "CRM", "DEV", "EDU", "ENV" \
+                                "HLH", "LEG", "MED", "MNC"]
+
+    # Filter data records that are of interest
+    df_news = df_news.filter(df_news.ActionGeo_CountryCode == 'US')
+    df_news = df_news.filter(df_news.Actor1Code != 'null')
+    df_news = df_news.filter(df_news.Actor1Type1Code.isin(role_codes_of_interest))
+
+    # There isn't a specific column specifying the state that the event happens in
+    #   The state is tied with the country code
+    #   So, parse out the state and make it a new column
+    name = 'ActionGeo_ADM1Code'
+    udf = UserDefinedFunction(lambda x: x[:2]+'-'+x[2:], StringType())
+    df_news = df_news.select(*[udf(column).alias(name) if column == name else \
+                                column for column in df_news.columns])
+    split_col = F.split(df_news['ActionGeo_ADM1Code'],'-')
+    df_news = df_news.withColumn('action_state', split_col.getItem(1))
+
+    # The GoldsteinScale's range is between -10 and 10, but it's mostly sparse
+    #   It's hard for a user to make use of it
+    #   The scale is normalized and served as a scale between 0 to 100.
+    #   Higher value denotes a more positive outcome
+    name = 'GoldsteinScale'
+    min_scale, max_scale = -10.0, 10.0
+    norm = UserDefinedFunction(lambda x: (x - min_scale)/(max_scale - \
+                                min_scale), DoubleType())
+    df_news = df_news.select(*[norm(col).cast(DoubleType()) \
+                .alias('normg_scale') if col == name else col for col in \
+                    df_news.columns])
+
+    df_news = df_news.filter(df_news.action_state != '')
+
+
+def aggregate_job(df_news):
+
+    df_news = df_news.groupby('action_state','Year','Actor1Type1Code') \
+                    .agg( \
+                        F.approx_count_distinct('GLOBALEVENTID').alias('events_count'),
+                        F.sum('normg_scale').alias('norm_score_cale'))
+                        #Calculate avg by dividing by event count
+
+    print finalized_df.show(finalized_df.count())
+    print finalized_df.printSchema()
+
+    return df_news
 
 if __name__ == "__main__":
 
@@ -45,109 +116,38 @@ if __name__ == "__main__":
         .config("spark.jars", "/home/ubuntu/Insight-GDELT-Feed/spark/postgresql-42.2.5.jar").getOrCreate()
 
     sqlcontext = SQLContext(sc)
-    gdelt_bucket = "s3n://gdelt-open-data/v2/events/20180410190000.export.csv"
-    gdelt_bucket2 = "s3n://gdelt-open-data/events/200601.csv"
-    #gdelt_bucket = "s3n://gdelt-open-data/v2/events/20190206*.export.csv"
-    #gdelt_bucket = "s3n://gdelt-open-data/v2/events/201*.export.csv"
 
-    df = sqlcontext.read \
-    	.format('com.databricks.spark.csv') \
-    	.options(header='false') \
-    	.options(delimiter="\t") \
-	.load(gdelt_bucket, schema = gdelt_schema_v2.gdeltSchema) \
- 
+    gdelt_bucket1 = "s3n://gdelt-open-data/events/[1,2][0-9]*[0-9][0-3|9].csv"
+    gdelt_bucket2 = "s3n://gdelt-open-data/events/*.export.csv"
+    #gdelt_bucket1 = "s3n://gdelt-open-data/events/200806.csv"
+    #gdelt_bucket2 = "s3n://gdelt-open-data/events/20150329.export.csv"
+
+    # Original datasets had a different schema from the new one
+    #   Thus, we need two different ways to read the data
+    df_1 = sqlcontext.read \
+        .format('com.databricks.spark.csv') \
+        .options(header='false') \
+        .options(delimiter="\t") \
+        .load(gdelt_bucket1, schema = gdelt_schema_v1.gdeltSchema)
+
     df_2 = sqlcontext.read \
         .format('com.databricks.spark.csv') \
         .options(header='false') \
         .options(delimiter="\t") \
-        .load(gdelt_bucket2, schema = gdelt_schema_v1.gdeltSchema)
-
-    sqlcontext.registerDataFrameAsTable(df, 'temp')
-    sqlcontext.registerDataFrameAsTable(df_2, 'temp2')
-
-    df_clean = sqlcontext.sql("""SELECT GLOBALEVENTID,
-                              CAST(SQLDATE AS INTEGER),
-                              MonthYear,
-                              Year,
-                              EventCode,
-                              Actor1Code,
-                              Actor1Name,
-                              Actor1Type1Code,
-                              ActionGeo_FullName,
-                              ActionGeo_CountryCode,
-			      Actor1Geo_CountryCode,
-                              ActionGeo_ADM1Code,
-			      Actor2Name,
-			      Actor2Geo_CountryCode,
-                              GoldsteinScale
-                            from temp
-                            """)
-
-    df_news = df_clean.select('GLOBALEVENTID','SQLDATE','MonthYear','Year','Actor1Code',
-                            'Actor1Type1Code','ActionGeo_FullName','ActionGeo_CountryCode',
-                            'ActionGeo_ADM1Code','Actor1Geo_CountryCode','GoldsteinScale')
-
-    df_clean2 = sqlcontext.sql("""SELECT GLOBALEVENTID,
-                            CAST(SQLDATE AS INTEGER),
-                            MonthYear,
-                            Year,
-                            EventCode,
-                            Actor1Code,
-                            Actor1Name,
-                            Actor1Type1Code,
-                            ActionGeo_FullName,
-                            ActionGeo_CountryCode,
-                            Actor1Geo_CountryCode,
-                            ActionGeo_ADM1Code,
-                            GoldsteinScale
-                            from temp2
-                            """)
-
-    df_clean2.show()
-
-    df_news2 = df_clean2.select('GLOBALEVENTID','SQLDATE','MonthYear','Year','Actor1Code',
-                            'Actor1Type1Code', 'ActionGeo_FullName', 'ActionGeo_CountryCode',
-                            'ActionGeo_ADM1Code','Actor1Geo_CountryCode','GoldsteinScale')
+        .load(gdelt_bucket2, schema = gdelt_schema_v2.gdeltSchema)
 
 
-    df_news = df_news.union(df_news2)
-
+    # Put together 2 sets of data with different schemas
+    df_v1 = get_clean_df(sqlcontext, df_1)
+    df_v2 = get_clean_df(sqlcontext, df_2)
+    df_news = df_v1.union(df_v2)
     df_news.show()
 
-                                    
-    role_codes = ["COP", "GOV", "JUD", "BUS", "CRM", "DEV", "EDU", "ENV" \
-                    "HLH", "LEG", "MED", "MNC"]
+    # Filter and aggregate data
+    processed_df = filter_df(df_news)
+    finalized_df = aggregate_job(processed_df)
 
-    df_news = df_news.filter(df_news.ActionGeo_CountryCode == 'US')
-    df_news = df_news.filter(df_news.Actor1Code != 'null')
-    df_news = df_news.filter(df_news.Actor1Type1Code.isin(role_codes))
+    # Write to database
+    postgres_dump(config, finalized_df)
 
-    name = 'ActionGeo_ADM1Code'
-    udf = UserDefinedFunction(lambda x: x[:2]+'-'+x[2:], StringType())
-    df_news = df_news.select(*[udf(column).alias(name) if column == name else column for column in df_news.columns])
-
-    name = 'GoldsteinScale'
-    min_scale, max_scale = -10.0, 10.0
-    norm = UserDefinedFunction(lambda x: (x - min_scale)/(max_scale - min_scale), DoubleType())
-    df_news = df_news.select(*[norm(col).cast(DoubleType()).alias('normg_scale') if col == name else col for col in df_news.columns])
-
-
-    split_col = F.split(df_news['ActionGeo_ADM1Code'],'-')
-    df_news = df_news.withColumn('action_state', split_col.getItem(1))
-    print df_news.show()
-
-    df_news = df_news.filter(df_news.action_state != '')
-
-    df_news = df_news.groupby('action_state','Year','Actor1Type1Code').agg( F.approx_count_distinct('GLOBALEVENTID').alias('events_count'),
-                                                                            #F.col('SQLDATE'),
-                                                                            #F.collect_list('EventCode'),
-                                                                            #F,avg('AvgTone').alias('avg_tone'),
-                                                                            F.sum('normg_scale').alias('norm_score_cale'))
-                                                                            #Calculate avg by dividing by event count
-
-    print df_news.show(df_news.count())
-    print df_news.printSchema()
-
-    #postgres_dump(config, df_news)
-
-
+    print 'Done'
