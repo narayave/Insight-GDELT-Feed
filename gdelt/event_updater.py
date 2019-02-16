ff4ade2ed378757dfe1c2849382e4f9ffb483440
Vedanth Narayanan <vedanth.nd@gmail.com>
2019-02-16 18:20:06 -0500

    Event adders and updaters sort of set up

--- gdelt/event_updater.py
+++ gdelt/event_updater.py
@@ -26,7 +26,7 @@ def get_df(con):
         """
 
     try:
-        dataframe=pd.read_sql_query(sql_query, con)
+        dataframe = pd.read_sql_query(sql_query, con)
     except Exception as e:
         print 'Error -' + str(e)
 
@@ -71,7 +71,8 @@ def normalize_goldstein(df):
     df = df.loc[df.goldsteinscale > -20.0]
 
     min_scale, max_scale = -10.000005, 10.000005
-    norm_gold = df['goldsteinscale'].apply(lambda x: (x - min_scale)/(max_scale - min_scale))
+    norm_gold = df['goldsteinscale'].apply(
+        lambda x: (x - min_scale) / (max_scale - min_scale))
 
     # print norm_gold
 
@@ -83,8 +84,13 @@ def normalize_goldstein(df):
 
 def clean_df(df):
 
-    df = df.drop(['actor1code','actiongeo_fullname', 'actiongeo_adm1code',
-                    'actor1geo_countrycode','sqldate','goldsteinscale'], axis=1)
+    df = df.drop(['actor1code',
+                  'actiongeo_fullname',
+                  'actiongeo_adm1code',
+                  'actor1geo_countrycode',
+                  'sqldate',
+                  'goldsteinscale'],
+                 axis=1)
 
     # pprint(df)
 
@@ -96,10 +102,10 @@ def aggregate_data(df):
     print 'In aggregate data function'
 
     df = df.groupby(['action_state', 'year', 'monthyear', 'actor1type1code'],
-        as_index=False).agg({
-            "globaleventid": ["count"],
-            "norm_scale": ["sum"]}).rename(columns={'globaleventid':'events_count',
-                            'norm_scale':'norm_scale_sum'})
+                    as_index=False).agg({
+                        "globaleventid": ["count"],
+                        "norm_scale": ["sum"]}).rename(columns={'globaleventid': 'events_count',
+                                                                'norm_scale': 'norm_scale_sum'})
 
     pattern = re.compile("^[a-zA-Z]+$")
     df = df.loc[df.action_state.str.contains(pattern)]
@@ -137,7 +143,6 @@ def batch_update(df, con):
     dict_df = df.to_dict(orient='records')
     # pprint(dict_df)
 
-
     cur = con.cursor()
 
     try:
@@ -166,6 +171,7 @@ def batch_update(df, con):
 
     return df
 
+
 if __name__ == '__main__':
 
     config = configparser.ConfigParser()
@@ -177,9 +183,13 @@ if __name__ == '__main__':
     dbhost = config.get('dbauth', 'host')
     dbport = config.get('dbauth', 'port')
 
-    db = create_engine('postgres://%s%s/%s'%(dbuser,dbhost,dbname))
+    db = create_engine('postgres://%s%s/%s' % (dbuser, dbhost, dbname))
     con = None
-    con = psycopg2.connect(database = dbname, host = dbhost, user = dbuser, password = dbpass)
+    con = psycopg2.connect(
+        database=dbname,
+        host=dbhost,
+        user=dbuser,
+        password=dbpass)
 
     df = get_df(con)
     df = get_states(df)
